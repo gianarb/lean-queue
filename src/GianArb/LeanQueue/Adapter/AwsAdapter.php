97f0889364755943ef4932c7239bb09a63477b9a
Gianluca Arbezzano <gianarb92@gmail.com>
2015-01-21 15:08:51 +0100

    Add AwsAdater
    New this lib support AwsSqs integration

---
+++ src/GianArb/LeanQueue/Adapter/AwsAdapter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace GianArb\LeanQueue\Adapter;
+
+class AwsAdapter implements AdapterInterface
+{
+    private $sqsClient;
+
+    public function __construct(\Aws\Sqs\SqsClient $sqsClient)
+    {
+        $this->sqsClient = $sqsClient;
+    }
+
+    public function send($queueName, $message)
+    {
+        $response = $this->sqsClient->sendMessage([
+            "QueueUrl" => $queueName,
+            "MessageBody" => $message,
+        ]);
+        return $response["MessageId"];
+    }
+
+    public function receive($queueName)
+    {
+        $response = $this->sqsClient->receiveMessage([
+            "QueueUrl" => $queueName
+        ]);
+
+        return [$response["Messages"][0]["Body"], $response["Messages"][0]["ReceiptHandle"]];
+    }
+}
