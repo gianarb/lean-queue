e337a503461dfaf834708d554fd2af58ad228f31
Gianluca Arbezzano <gianarb92@gmail.com>
2015-01-21 12:27:14 +0100

    Add ArrayAdapter
    ArrayAdapter to manage testing

---
+++ src/GianArb/LeanQueue/Adapter/ArrayAdapter.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace GianArb\LeanQueue\Adapter;
+
+class ArrayAdapter implements AdapterInterface
+{
+    private $data = [];
+
+    public function send($queue, $message)
+    {
+        if (!array_key_exists($queue, $this->data)) {
+            $this->data[$queue] = [];
+        }
+
+        $d = array(
+            'id' => rand(1e6, 10e6),
+            'content'    => $message,
+        );
+
+        $this->data[$queue][] = $d;
+        return $d['id'];
+    }
+
+    public function receive($queue)
+    {
+        if (!array_key_exists($queue, $this->data)) {
+            throw \Exception();
+        }
+        $row = $this->data[$queue->getName()][0];
+        return [$row['id'], $row['content']];
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+}
